a56bf5caedecab3870a2d49c87ada31fa536aabc
Marcus Legendre <marcus.legendre@gmail.com>
2017-10-02 02:43:37 +0200

    New first level, new colors, implemented lives

--- breakout.js
+++ breakout.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById("gamecanvas");
 const context = canvas.getContext("2d");
 document.addEventListener("keydown", keyPush);
-setInterval(game,1000/60);
+setInterval(loop,1000/60);
 
+const game = {
+    score: 0,
+    lives: 3,
+    level: null,
+    blocks: null,
+}
 
 const ball = {
     speed: -5,
@@ -20,23 +26,32 @@ const player = {
     moveRight: function() { this.pos.x += this.speed; },
 };
 
-function Block(x, y, width, height) {
+function Block(x, y, width, height, color="#FFE792") {
     this.pos = { x: x, y: y};
     this.width = width;
     this.height = height;
+    this.color = color;
+}
+
+function resetGame() {
+    game.score = 0;
+    game.lives = 3;
+    game.level = 1;
+    game.blocks = level1();
 }
 
 function level1() {
     const blocks = [];
-    for (i = 0; i < 6; i++) {
-        for (j = 0; j < 4; j++) {
-            blocks.push(new Block(96+i*80, 96+j*24, 64, 16, true));
+    const colors = ["#F92672", "#66D9EF", "#A6E22E", "#FD971F", "#AE81FF", "#FFE792", "#FFE792"];
+    for (row = 0; row < 7; row++) {
+        for (col = 0; col < 8; col++) {
+            blocks.push(new Block(2+col*60, 48+row*20, 58, 16, colors[row]));
         }
     }
     return blocks;
 }
 
-function game() {
+function loop() {
     // update ball position
     if (ball.pos.y-ball.size/2 > canvas.height) {
         // player missed the ball
@@ -44,6 +59,8 @@ function game() {
         ball.pos.y = canvas.height / 2 - ball.size / 2;
         ball.velocity.x = 0;
         ball.velocity.y = ball.speed;
+
+        if (--game.lives == 0) resetGame();
     } else if (ball.pos.y-ball.size/2 <= 0) {
         // ball touched ceiling
         ball.velocity.y = -ball.velocity.y;
@@ -62,39 +79,40 @@ function game() {
     }
 
     // detect ball collision with blocks
-    blocks.forEach(function(block) {
+    game.blocks.forEach(function(block) {
         if (ball.pos.x-ball.size/2 < block.pos.x+block.width && ball.pos.x+ball.size/2 > block.pos.x
             && ball.pos.y-ball.size/2 < block.pos.y+block.height && ball.pos.y+ball.size/2 > block.pos.y) {
-                var i = blocks.indexOf(block);
-                blocks.splice(i, 1);
-                score++;
+                var i = game.blocks.indexOf(block);
+                game.blocks.splice(i, 1);
+                game.score++;
                 ball.velocity.x = -ball.velocity.x;
                 ball.velocity.y = -ball.velocity.y;
         }
     });
     
     // draw background
-    context.fillStyle = "black";
+    context.fillStyle = "#272822";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
 	// draw ball
-    context.fillStyle = "white";
+    context.fillStyle = "#F8F8F2";
     context.fillRect(ball.pos.x-ball.size/2, ball.pos.y-ball.size/2, ball.size, ball.size);
 
     // draw player
-	context.fillStyle = "red";
+	context.fillStyle = "#F92672";
     context.fillRect(player.pos.x-player.width/2, player.pos.y, player.width, 16);
 
     // draw blocks
-    context.fillStyle = "blue";
-    blocks.forEach(function(block) {
+    game.blocks.forEach(function(block) {
+        context.fillStyle = block.color;
         context.fillRect(block.pos.x, block.pos.y, block.width, block.height);
     });
 
-    // draw score
+    // draw score and lives
     context.fillStyle = "white";
     context.font = "20px Georgia";
-    context.fillText("Score: " + score, 10, 20);
+    context.fillText("Score: " + game.score, 10, 20);
+    context.fillText("Lives: " + game.lives, canvas.width-80, 20);
 
     // AI
     /*if (player.pos.x-ball.pos.x < -20) {
@@ -114,6 +132,4 @@ function keyPush(evt) {
 	}
 }
 
-
-var score = 0;
-var blocks = level1();
+resetGame();
