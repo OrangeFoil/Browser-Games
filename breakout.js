2a180298426060e4108ececc03e0c90741d777e7
Marcus Legendre <marcus.legendre@gmail.com>
2017-10-02 04:41:50 +0200

    Refactored collision detection

--- breakout.js
+++ breakout.js
@@ -6,12 +6,14 @@ setInterval(loop,1000/60);
 const game = {
     demoMode: null,
     score: null,
+    multiplier: null,
     lives: null,
     level: null,
     blocks: null,
     reset: function() {
         this.demoMode = true;
         this.score = 0;
+        this.multiplier = 1;
         this.lives = 3;
         this.level = 1;
         this.blocks = generateLevel(this.level);
@@ -21,11 +23,12 @@ const game = {
 const ball = {
     pos: {x: null, y: null},
     velocity: {x: null, y: null },
-    size: null,
+    width: null,
+    height: null,
     speed: null,
     draw: function() {
         context.fillStyle = "#F8F8F2";
-        context.fillRect(this.pos.x-this.size/2, this.pos.y-this.size/2, this.size, this.size);
+        context.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     },
     reset: function() {
         this.pos.x = canvas.width / 2;
@@ -34,7 +37,7 @@ const ball = {
         this.velocity.x = (Math.random() + 0.5) * Math.pow(-1, Math.floor(Math.random() * 2));
         if (game.demoMode) this.velocity.x += 2;
         this.velocity.y = this.speed;
-        this.size = 12;
+        this.width = this.height = 12;
     },
 };
 
@@ -45,15 +48,15 @@ const player = {
     speed: null,
     draw: function() {
         context.fillStyle = "#F92672";
-        context.fillRect(this.pos.x-this.width/2, this.pos.y, this.width, 16);
+        context.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     },
     moveLeft: function() { if (this.pos.x >= 0) this.pos.x -= this.speed; },
     moveRight: function() { if (this.pos.x <= canvas.width) this.pos.x += this.speed; },
     reset: function() {
-        this.pos.x = canvas.width / 2;
-        this.pos.y = canvas.height - 32;
         this.width = 120;
-        this.height = 32;
+        this.height = 16;
+        this.pos.x = canvas.width / 2 - this.width / 2;
+        this.pos.y = canvas.height - 32;
         this.speed = 20;
     },
 };
@@ -67,6 +70,16 @@ function Block(x, y, width, height, color="#FFE792") {
         context.fillStyle = this.color;
         context.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     };
+}
+
+function collisionDetection(a, b) {
+    if (a.pos.x < b.pos.x + b.width &&
+        a.pos.x + a.width > b.pos.x &&
+        a.pos.y < b.pos.y + b.height &&
+        a.height + a.pos.y > b.pos.y) {
+        return true;
+    }
+    return false;
 }
 
 function generateLevel(level) {
@@ -93,15 +106,16 @@ function generateLevel(level) {
 
 function loop() {
     // update ball position
-    if (ball.pos.y-ball.size/2 > canvas.height) {
+    if (ball.pos.y-ball.height/2 > canvas.height) {
         // player missed the ball
         ball.reset();
         player.reset();
+        game.multiplier = 1;
         if (--game.lives == 0) game.reset();
-    } else if (ball.pos.y-ball.size/2 <= 0) {
+    } else if (ball.pos.y-ball.height/2 <= 0) {
         // ball touched ceiling
         ball.velocity.y = -ball.velocity.y;
-    } else if (ball.pos.x-ball.size/2 <= 0 || ball.pos.x+ball.size/2 >= canvas.width) {
+    } else if (ball.pos.x-ball.width/2 <= 0 || ball.pos.x+ball.width/2 >= canvas.width) {
         // ball touched left or right wall
         ball.velocity.x = -ball.velocity.x;
     }
@@ -109,19 +123,18 @@ function loop() {
     ball.pos.y += ball.velocity.y;
 
     // detect ball collision with player
-    if (ball.pos.x+ball.size/2 >= player.pos.x-player.width/2 && ball.pos.x-ball.size/2 <= player.pos.x+player.width/2
-    && ball.pos.y-ball.size/2 >= player.pos.y-player.height/2 && ball.pos.y+ball.size/2 <= player.pos.y+player.height) {
+    if (collisionDetection(ball, player)) {
         ball.velocity.x = (ball.pos.x - player.pos.x) / 10;
         ball.velocity.y = -ball.velocity.y;
+        game.multiplier = 1;
     }
 
     // detect ball collision with blocks
     game.blocks.forEach(function(block) {
-        if (ball.pos.x-ball.size/2 < block.pos.x+block.width && ball.pos.x+ball.size/2 > block.pos.x
-            && ball.pos.y-ball.size/2 < block.pos.y+block.height && ball.pos.y+ball.size/2 > block.pos.y) {
+        if (collisionDetection(ball, block)) {
                 var i = game.blocks.indexOf(block);
                 game.blocks.splice(i, 1);
-                game.score++;
+                game.score += ++game.multiplier;
                 ball.velocity.x = -ball.velocity.x;
                 ball.velocity.y = -ball.velocity.y;
         }
@@ -158,9 +171,9 @@ function loop() {
 
     if (game.demoMode) {
         // AI
-        if (player.pos.x-ball.pos.x < -20) {
+        if ((player.pos.x + player.width/2) - (ball.pos.x + ball.width/2) < player.speed) {
             player.moveRight();
-        } else if (player.pos.x-ball.pos.x > 20) {
+        } else if ((player.pos.x + player.width/2) - (ball.pos.x + ball.width/2) > -1*player.speed) {
             player.moveLeft();
         }
 
