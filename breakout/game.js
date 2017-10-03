ec81bc41beed4a66063b053aba9fad72d8944220
Marcus Legendre <marcus.legendre@gmail.com>
2017-10-03 16:29:02 +0200

    Moved generateLevel() into Game class

--- breakout/game.js
+++ breakout/game.js
@@ -35,7 +35,7 @@ class Game {
 
         this.ball = new Ball();
         this.player = new Player();
-        this.blocks = generateLevel(this.level);
+        this.blocks = this.generateLevel(this.level);
     }
 
     startGame() {
@@ -47,7 +47,7 @@ class Game {
 
         this.ball = new Ball();
         this.player = new Player();
-        this.blocks = generateLevel(this.level);
+        this.blocks = this.generateLevel(this.level);
     }
 
     initializeSounds() {
@@ -136,7 +136,7 @@ class Game {
                 this.startAttractMode();
             } else {
                 // move to next level
-                this.blocks = generateLevel(++this.level);
+                this.blocks = this.generateLevel(++this.level);
                 this.ball = new Ball();
                 this.player = new Player();
             }
@@ -185,5 +185,71 @@ class Game {
             context.fillText("Lives: " + this.lives, canvas.width-10, 20);
             context.globalAlpha = 1;
         }
+    }
+
+    generateLevel(level) {
+        const blocks = [];
+        const colors = ["#F92672", "#66D9EF", "#A6E22E", "#FD971F", "#AE81FF", "#FFE792", "#FFE792"];
+        var row, col;
+        if (level == 1) {
+            for (row = 0; row < 7; row++) {
+                for (col = 0; col < 8; col++) {
+                    blocks.push(new Block(2+col*60, 60+row*20, 58, 16, colors[row]));
+                }
+            }
+        } else if (level == 2) {
+            for (row = 0; row < 3; row++) {
+                for (col = 1; col < 7; col++) {
+                    blocks.push(new Block(2+col*60, 60+row*20, 58, 16, colors[row]));
+                }
+            }
+
+            for (row = 0; row < 4; row++) {
+                for (col = 0; col < 8; col++) {
+                    if (col == 2 || col == 5) continue;
+                    blocks.push(new Block(2+col*60, 160+row*20, 58, 16, colors[row]));
+                }
+            }
+
+            for (col = 0; col < 8; col++) {
+                blocks.push(new Block(2+col*60, 160+row*20, 58, 16, "grey", 2));
+            }
+        } else if (level == 3) {
+            blocks.push(new Block(60, 60, 16, 196, "grey", 2));
+            blocks.push(new Block(canvas.width-76, 60, 16, 196, "grey", 2));
+
+            for (row = 0; row < 3; row++) {
+                for (col = 0; col < 6; col++) {
+                    blocks.push(new Block(80+col*54, 60+row*20, 50, 16, colors[row]));
+                }
+            }
+            for (row = 0; row < 4; row++) {
+                blocks.push(new Block(80, 120+row*20, 50, 16, colors[2]));
+                blocks.push(new Block(canvas.width-130, 120+row*20, 50, 16, colors[2]));
+            }
+
+            for (col = 0; col < 2; col++) {
+                blocks.push(new Block(134+col*108, 120, 104, 76, colors[5]));
+            }
+
+            for (row = 0; row < 3; row++) {
+                for (col = 0; col < 6; col++) {
+                    blocks.push(new Block(80+col*54, 200+row*20, 50, 16, colors[2-row]));
+                }
+            }
+
+            for (col = 0; col < 6; col++) {
+                blocks.push(new Block(80+col*54, 200+row*20, 50, 16, "grey", 2));
+            }
+        } else {
+            // simple random level
+            for (row = 0; row < 7; row++) {
+                for (col = 0; col < 8; col++) {
+                    if (Math.random() >= 0.5) continue;
+                    blocks.push(new Block(2+col*60, 60+row*20, 58, 16, colors[row]));
+                }
+            }
+        }
+        return blocks;
     }
 }
